bc8c845ac8bff6127be26c18a6464500187a0552
WallysonCastro <luanrodrigues007@hotmail.com>
2019-12-04 17:13:38 -0300

    arrumar chamada recursiva do selection

--- sort/selection_sort.cpp
+++ sort/selection_sort.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+using namespace std;
+
+
 /**
  * Selection sort: sort onde procura o menor e
  * coloca ele na primeira posicao, e assim sucessivamente
@@ -37,11 +41,11 @@ void selection_sort(int v[], int tam) {
  * 
  */
 void selection_sort_recursivo(int v[], int tam) {
-	if (tam == -1) 
+	if (tam <= 1) 
 		return;
 	
 	int menor = 0;
-	for (int i = 0; i < tam; i++) {
+	for (int i = 1; i < tam; i++) {
 		if (v[i] < v[menor])
 			menor = i;
 	}
@@ -50,5 +54,21 @@ void selection_sort_recursivo(int v[], int tam) {
 	v[0] = v[menor];
 	v[menor] = aux;
 	
-	selection_sort(&v[1], tam - 1);
+	selection_sort_recursivo(&v[1], tam - 1);
+}
+
+int main()
+{
+	
+	int v[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	int tam = 9;
+	
+	selection_sort_recursivo(v, tam);
+	
+	for (int i = 0; i < tam; i ++)
+		cout << v[i] << " ";
+	cout << endl;
+	
+	
+	return 0;
 }
